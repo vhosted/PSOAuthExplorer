34db54c9b7665593c6d2686f8036460719f3dd5c
Janick Marbot <janick.marbot@gmail.com>
2024-12-19 14:21:31 +0100

    chore: add output

--- build/build.ps1
+++ build/build.ps1
@@ -12,6 +12,8 @@ $manifest = Get-ChildItem -Recurse | Where-Object { $_.Name -like "*.psd1" }
 # get public functions to export
 $pubFunctions = ((Get-ChildItem *ps1 -Recurse | Where-Object { $_.DirectoryName -like "*public*" }).Name).Replace(".ps1", "")
 
+Write-Output "updating module manifest $($manifest.FullName) to version $Version"
+
 if ($PreReleaseTag) {
     Update-ModuleManifest -Path $manifest.FullName -FunctionsToExport $pubFunctions -ModuleVersion $Version -Prerelease $PreReleaseTag
 }
