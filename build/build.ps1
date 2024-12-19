645ec4c19b25bc06a66a0910eb96c0caf00cfcd2
Janick Marbot <janick.marbot@gmail.com>
2024-12-19 17:52:23 +0100

    fix: various fixes

--- build/build.ps1
+++ build/build.ps1
@@ -1,20 +1,14 @@
 [CmdletBinding()]
 param (
-    [Parameter(Mandatory)]
-    [string]$ModulePath,
     [version]$Version,
     [string]$PreReleaseTag
 
 )
 
-Write-Output (Get-ChildItem -Recurse)
-
 $manifest = Get-ChildItem -Recurse | Where-Object { $_.Name -like "*.psd1" }
 
 # get public functions to export
 $pubFunctions = ((Get-ChildItem *.ps1 -Recurse | Where-Object { $_.DirectoryName -like "*Public*" }).Name).Replace(".ps1", "")
-
-Write-Output "updating module manifest $($manifest.FullName) to version $Version"
 
 if ($PreReleaseTag) {
     Update-ModuleManifest -Path $manifest.FullName -FunctionsToExport $pubFunctions -ModuleVersion $Version -Prerelease $PreReleaseTag
@@ -22,3 +16,5 @@ if ($PreReleaseTag) {
 else {
     Update-ModuleManifest -Path $manifest.FullName -FunctionsToExport $pubFunctions -ModuleVersion $Version
 }
+
+Write-Output "updated module manifest $($manifest.FullName) to version $Version"
