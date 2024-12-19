a3e888b7676f592ea2d7065ced0dcc9360f86772
Janick Marbot <janick.marbot@gmail.com>
2024-12-19 14:17:24 +0100

    fix: path issues

--- build/build.ps1
+++ build/build.ps1
@@ -10,7 +10,7 @@ param (
 $manifest = Get-ChildItem -Recurse | Where-Object { $_.Name -like "*.psd1" }
 
 # get public functions to export
-$pubFunctions = ((Get-ChildItem "$($manifest.DirectoryName)\public\*.ps1" -Recurse).Name).Replace(".ps1", "")
+$pubFunctions = ((Get-ChildItem *ps1 -Recurse | Where-Object { $_.DirectoryName -like "*public*" }).Name).Replace(".ps1", "")
 
 if ($PreReleaseTag) {
     Update-ModuleManifest -Path $manifest.FullName -FunctionsToExport $pubFunctions -ModuleVersion $Version -Prerelease $PreReleaseTag
