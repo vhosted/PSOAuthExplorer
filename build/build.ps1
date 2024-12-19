fa3a161af059e749b5a616d54a9bbba42f64d89a
Janick Marbot <janick.marbot@gmail.com>
2024-12-19 14:30:01 +0100

    fix: path

--- build/build.ps1
+++ build/build.ps1
@@ -12,7 +12,7 @@ Write-Output (Get-ChildItem -Recurse)
 $manifest = Get-ChildItem -Recurse | Where-Object { $_.Name -like "*.psd1" }
 
 # get public functions to export
-$pubFunctions = ((Get-ChildItem *ps1 -Recurse | Where-Object { $_.DirectoryName -like "*public*" }).Name).Replace(".ps1", "")
+$pubFunctions = ((Get-ChildItem *.ps1 -Recurse | Where-Object { $_.DirectoryName -like "*Public*" }).Name).Replace(".ps1", "")
 
 Write-Output "updating module manifest $($manifest.FullName) to version $Version"
 
